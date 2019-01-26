e00860ae8a70994ffdae8bdb92bd6d35e225ae6a
Ritchy <blezin.ritchy@gmail.com>
2019-01-26 22:47:37 +0100

    Ajout des images: hero, bandit, mur.

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,7 @@
 
 FILE_LEVEL = 'levels/level_1.txt'
 FLOOR_IMG = 'images/floor_1.png'
+HERO_IMG = 'images/MacGyver.png'
+BANDIT_IMG = 'images/Gardien.png'
+WALL_IMG = 'images/wall_1.png'
 WINDOW_SIZE = (640, 480)
