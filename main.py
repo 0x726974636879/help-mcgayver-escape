b0254a3b1514e9cbcc2de0cd99ebdcf154c795bb
Ritchy <blezin.ritchy@gmail.com>
2019-01-26 15:48:22 +0100

    Initialisation de la base du programme.

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+from constants import (
+	WINDOW_SIZE
+)
+
+# Initialize pygame
+pygame.init()
+# Create the window
+window = pygame.display.set_mode(WINDOW_SIZE)
+# Condition loop for the game continue
+play = True
+
+while play:
+	continue
