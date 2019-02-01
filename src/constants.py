7718db522aac94abd351feb8fde50b7634e99ed3
Ritchy <blezin.ritchy@gmail.com>
2019-02-01 23:15:46 +0100

    Ajout des images : ether, aiguille, tube en plastique et de la seringue

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,12 @@
 # Images path
 #############
 ENEMY_IMG = 'images/Gardien.png'
+ETHER_IMG = 'images/ether.png'
 FLOOR_IMG = 'images/floor_1.png'
 HERO_IMG = 'images/MacGyver.png'
+NEEDLE_IMG = 'images/aiguille.png'
+PLASTIC_TUBE_IMG = 'images/tube_plastique.png'
+SYRINGE_IMG = 'images/seringue.png'
 WALL_IMG = 'images/wall_1.png'
 
 #############
