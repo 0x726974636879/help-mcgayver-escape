06e9f3842f2e82fa9260b17939144cefa46c90c5
Ritchy <blezin.ritchy@gmail.com>
2019-02-01 17:56:01 +0100

    Remplacement de methode par des propriétés pour la position actuelle du character

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import pygame
-
 from constants import (
     ENEMY_IMG,
     FILE_LEVEL,
@@ -22,12 +21,11 @@ level = Level(FILE_LEVEL)
 characters_position = level.characters_position
 walls_position = level.walls_position
 
-
 # Initialize the characters.
-main_character = Character(HERO_IMG)
-main_character_position = characters_position['start']
-enemy = Character(ENEMY_IMG)
-enemy_position = characters_position['end']
+main_character = Character(HERO_IMG, walls_position)
+main_character.position = characters_position['start']
+enemy = Character(ENEMY_IMG, walls_position)
+enemy.position = characters_position['end']
 
 # Condition loop for the game continue
 play = True
@@ -40,14 +38,13 @@ while play:
             play = False
         if event.type == pygame.KEYDOWN:
             # Change the position of the main character.
-            main_character_position = main_character.move_character(
-                event.key, main_character_position)
+            main_character.move_character(event.key)
 
     # Show the level.
     level.show_level(window)
     # Show the main character.
-    main_character.show_character(window, main_character_position)
-    enemy.show_character(window, enemy_position)
+    main_character.show_character(window)
+    enemy.show_character(window)
 
     # Re-initialize the display.
     pygame.display.flip()
