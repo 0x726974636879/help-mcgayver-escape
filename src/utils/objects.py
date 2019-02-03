a024e9af8b8c096bc77aa07cc034ab2f87e5c6b7
Ritchy <blezin.ritchy@gmail.com>
2019-02-03 17:03:55 +0100

    Implémentation des images: 'ether', 'needle', 'plastic_tube', 'syringe', Création de la méthode: 'init_objects', mise en place des objects nécessaires à la réussite du niveau.

--- src/utils/objects.py
+++ src/utils/objects.py
@@ -1,8 +1,12 @@
 import pygame
 
 from constants import (
+    ETHER_IMG,
     FLOOR_IMG,
+    NEEDLE_IMG,
+    PLASTIC_TUBE_IMG,
     SPRITE_SIZE,
+    SYRINGE_IMG,
     WALL_IMG,
     WINDOW_SIZE
 )
@@ -17,6 +21,7 @@ class Level:
     def __init__(self, file):
         self.level_structure = self.generate_level(file)
         self.characters_position = None
+        self.objects_image = {}
         self.walls_position = None
         self.get_initial_positions()
 
@@ -85,27 +90,59 @@ class Level:
 
         :param window: pygame display object.
         """
-        # Initialiaze all images make them to the good size.
-        floor = pygame.transform.scale(
-            pygame.image.load(FLOOR_IMG).convert(),
-            (SPRITE_SIZE, SPRITE_SIZE)
-        )
-        wall = pygame.transform.scale(
-            pygame.image.load(WALL_IMG).convert(),
-            (SPRITE_SIZE, SPRITE_SIZE)
-        )
         # Run through the level structure list to put
         # each image at the good place.
+        self.init_objects()
         for y, row in enumerate(self.level_structure):
             for x, letter in enumerate(row):
                 position = (
                     SPRITE_SIZE * x,
                     SPRITE_SIZE * y
                 )
-                if letter == 'e' or letter == 'f' or letter == 's':
-                    window.blit(floor, position)
+                if (letter == 'e' or letter == 'f' or letter == 's' or
+                    letter == '1' or letter == '2' or letter == '3' or
+                        letter == '4'):
+                    window.blit(self.objects_image['floor'], position)
+                    if letter == '1':
+                        window.blit(self.objects_image['ether'], position)
+                    elif letter == '2':
+                        window.blit(self.objects_image['needle'], position)
+                    elif letter == '3':
+                        window.blit(
+                            self.objects_image['plastic_tube'], position)
+                    elif letter == '4':
+                        window.blit(self.objects_image['syringe'], position)
                 elif letter == 'w':
-                    window.blit(wall, position)
+                    window.blit(self.objects_image['wall'], position)
+
+    def init_objects(self):
+        """
+        Initialiaze all images make them to the good size.
+        """
+        self.objects_image['ether'] = pygame.transform.scale(
+            pygame.image.load(ETHER_IMG).convert_alpha(),
+            (SPRITE_SIZE, SPRITE_SIZE)
+        )
+        self.objects_image['floor'] = pygame.transform.scale(
+            pygame.image.load(FLOOR_IMG).convert(),
+            (SPRITE_SIZE, SPRITE_SIZE)
+        )
+        self.objects_image['needle'] = pygame.transform.scale(
+            pygame.image.load(NEEDLE_IMG).convert_alpha(),
+            (SPRITE_SIZE, SPRITE_SIZE)
+        )
+        self.objects_image['plastic_tube'] = pygame.transform.scale(
+            pygame.image.load(PLASTIC_TUBE_IMG).convert_alpha(),
+            (SPRITE_SIZE, SPRITE_SIZE)
+        )
+        self.objects_image['syringe'] = pygame.transform.scale(
+            pygame.image.load(SYRINGE_IMG).convert_alpha(),
+            (SPRITE_SIZE, SPRITE_SIZE)
+        )
+        self.objects_image['wall'] = pygame.transform.scale(
+            pygame.image.load(WALL_IMG).convert(),
+            (SPRITE_SIZE, SPRITE_SIZE)
+        )
 
 
 class Character:
