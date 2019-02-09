33724944b5910d7b0483bd78001ac1462588860d
Ritchy <blezin.ritchy@gmail.com>
2019-02-09 01:13:50 +0100

    Suppression de l'ancienne aiguille, peu visible.

--- src/constants.py
+++ src/constants.py
@@ -5,9 +5,8 @@ ENEMY_IMG = 'images/Gardien.png'
 ETHER_IMG = 'images/ether.png'
 FLOOR_IMG = 'images/floor_1.png'
 HERO_IMG = 'images/MacGyver.png'
-NEEDLE_IMG = 'images/aiguille.png'
 PLASTIC_TUBE_IMG = 'images/tube_plastique.png'
-SYRINGE_IMG = 'images/seringue.png'
+NEEDLE_IMG = 'images/aiguille.png'
 WALL_IMG = 'images/wall_1.png'
 
 #############
