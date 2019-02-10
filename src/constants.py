3fb395b0dc4fdd0a5f86f25c0f3b21cda9fb3094
Ritchy <blezin.ritchy@gmail.com>
2019-02-10 21:08:59 +0100

    Ajout du choix automatique du sol, du niveau et des murs.

--- src/constants.py
+++ src/constants.py
@@ -1,19 +1,26 @@
+from random import randint
+
+floor_number = randint(1, 2)
+level_number = randint(1, 2)
+wall_number = randint(1, 2)
+
+
 #############
 # Images path
 #############
 BACKGROUNG_IMG = 'images/background_img.png'
 ENEMY_IMG = 'images/Gardien.png'
 ETHER_IMG = 'images/ether.png'
-FLOOR_IMG = 'images/floor_1.png'
+FLOOR_IMG = f'images/floor_{floor_number}.png'
 HERO_IMG = 'images/MacGyver.png'
 PLASTIC_TUBE_IMG = 'images/tube_plastique.png'
 NEEDLE_IMG = 'images/aiguille.png'
-WALL_IMG = 'images/wall_1.png'
+WALL_IMG = f'images/wall_{wall_number}.png'
 
 #############
 # Level
 #############
-FILE_LEVEL = 'levels/level_2.txt'
+FILE_LEVEL = f'levels/level_{level_number}.txt'
 
 ############
 # Others
